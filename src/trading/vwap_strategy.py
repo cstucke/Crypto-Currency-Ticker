d83762c4d8d2d047d549bae8d4893840c21e1ac4
Charles Stucke <charles-otto.stucke@epita.fr>
2026-01-04 14:01:06 -0600

    Initial commit for VWAP Trading strategy. Empty class and imports implemented

---
+++ src/trading/vwap_strategy.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from src.trading.strategy import TradingStrategy
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+class VWAPStrategy(TradingStrategy):
+    pass
